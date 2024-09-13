925cc135b4725c8ce71be22780e7718616256769
fanbyprinciple <fanbyprinciple@gmail.com>
2024-09-13 18:11:47 +0530

    push on 2024-09-14 18:11:47.293495

--- github_push_automator_script/automator.py
+++ github_push_automator_script/automator.py
@@ -3,10 +3,18 @@ import datetime
 
 x= datetime.datetime.now()
 
+how_many_commits = 7 # commits 7 days before today
 text = "push on"
+# commit_file = open("commit.txt", "w")
+# os.system('cmd /c "git add ."')
+i = 1
+while(i < 7):
+    with open("commit_file", "w") as commit_file:
+        commit_file.write(str(i))
+    os.system('cmd /c "git add ."')
+    os.system(f'cmd /c "git commit -m \"{text} {x}\" --date=\"{i} days ago\"')
 
-os.system('cmd /c "git add ."')
-os.system(f'cmd /c "git commit -m \"{text} {x}\"')
 os.system('cmd /c "git push"')
+
 
 
